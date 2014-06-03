cf8d6ec5d1f7ca63e1a1e7f9742403d0ed6c5e37
Ryan Bourgeois <bluedragonx@gmail.com>
2014-06-02 22:39:30 -0700

    Add connection/collection functions to metadata class. Be consistent with bind method naming. Add an _id field to docuemnts when necessary.

--- minimongo/meta.py
+++ minimongo/meta.py
@@ -1,16 +1,22 @@
+"""Meta functionality used for document creation."""
+from .connection import find_connection
+from .errors import DocumentError
 from .field import Field
+from bson import ObjectId
 from collections import defaultdict
+from utils import to_snake_case
 
 
 class DocumentMeta(object):
     """Metadata container for Document classes."""
 
     def __init__(self, cls, attrs, meta):
+        """Initialize new document class metadata."""
         self.cls = cls
         self.fields = {}
         self.options = {}
 
-        cls.__init__ = self.init_hook()
+        self.bind_init()
 
         if attrs:
             if not hasattr(attrs, '__getitem__'):
@@ -18,12 +24,22 @@ class DocumentMeta(object):
             for name, attr in attrs.items():
                 if isinstance(attr, Field):
                     self.fields[name] = attr
+
         if meta:
-            self.options = vars(meta)
+            self.options.update(vars(meta))
 
-        self.bind()
+        self._connection = self.options.pop('connection', None)
+        self._collection = self.options.pop('collection', None)
+        if not self._collection:
+            self._collection = to_snake_case(cls.__name__)
 
-    def init_hook(meta):
+        if self._connection and '_id' not in fields:
+            self.fields['_id'] = Field(ObjectId)
+
+        self.bind_fields()
+
+    def bind_init(meta):
+        """Bind init hook to the document class."""
         parent = meta.cls.__init__
         # prevent recursive decoration
         if hasattr(parent, 'parent'):
@@ -37,9 +53,10 @@ class DocumentMeta(object):
         __init__.name = parent.__name__
         __init__.hook = True
         __init__.parent = parent
-        return __init__
+        self.cls.__init__ = __init__
 
-    def bind(self):
+    def bind_fields(self):
+        """Bind fields to the document class."""
         defaults = {}
         for name, field in self.fields.items():
             setattr(self.cls, name, field(name))
@@ -47,13 +64,28 @@ class DocumentMeta(object):
 
         self.defaults = defaults
 
+    def get_connection(self):
+        """Return the connection associated with this document."""
+        if self._connection:
+            return find_connection(self._connection)
+        return None
+
+    def get_collection(self, connection=None):
+        """Return the collection associated with this document."""
+        connection = connection or self.get_connection()
+        if connection:
+            return connection[self._collection]
+        return None
+
     def __repr__(self):
-        return '<Meta ({})>'.format(self.cls.__name__)
+        return '<DocumentMeta ({})>'.format(self.cls.__name__)
 
 
 class DocumentBuilder(type):
     """Metaclass for building document classes."""
+
     def __new__(meta, name, bases, attrs):
+        """Create and attach metadata to the document."""
         Meta = attrs.pop('Meta', {})
         cls = type.__new__(meta, name, bases, attrs)
         cls._meta = DocumentMeta(cls, attrs, Meta)
---
+++ minimongo/utils.py
@@ -0,0 +1,8 @@
+"""A few useful utilities."""
+import re
+
+
+def to_snake_case(name):
+    """Convert a camel case string to snake case."""
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
