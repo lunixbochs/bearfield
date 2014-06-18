cc818d23942607edb2d481533f9b648f3572afcf
Ryan Bourgeois <bluedragonx@gmail.com>
2014-06-18 14:03:17 -0700

    Add errors to __init__ exports.

--- bearfield/__init__.py
+++ bearfield/__init__.py
@@ -1,17 +1,31 @@
 from .connection import Connection, configure
 from .document import Document
+from .errors import (
+    ConfigError,
+    DocumentError,
+    EncodingError,
+    Error,
+    OperationError,
+    ValidationError,
+)
 from .field import Field
 from .query import Q, Query
 from .reference import Reference
 from bson import ObjectId
 
 __all__ = [
+    'ConfigError',
     'Connection',
     'Document',
+    'DocumentError',
+    'EncodingError',
+    'Error',
     'Field',
     'ObjectId',
+    'OperationError',
     'Q',
     'Query',
     'Reference',
+    'ValidationError',
     'configure',
 ]
